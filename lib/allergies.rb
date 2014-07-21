43bd6a39821bd3e593296d4b2ee59b4c6f19f05f
Sanjeev & Mark R <student@epicodus.com>
2014-07-21 13:56:56 -0700

    Updated allergy tracker by using while loop to iterate through until value is 0

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,8 +1,24 @@
 def allergies(num)
   scoreArray = []
   allergydict = {
-  8 => 'strawberries'
+  1 => 'eggs',
+  2 => 'peanuts',
+  4 => 'shellfish',
+  8 => 'strawberries',
+  16 => 'tomatoes',
+  32 => 'chocolate',
+  64 => 'pollen',
+  128 => 'cats',
   }
- scoreArray.push(allergydict[num])
+  while num != 0
+    allergydict.each do |score, allergy|
+      if num / score == 1
+        scoreArray.push(allergy)
+        num -= score
+      end
+    end
+  end
+  scoreArray
 end
-puts allergies(8)
+
+puts allergies(255)
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -2,7 +2,10 @@ require ('rspec')
 require ('allergies')
 
 describe('allergies') do
-  it ('put in a number and get back the closest allergy') do
+  it ('put in a number and get back the corresponding allergy') do
     allergies(8).should(eq(['strawberries']))
+  end
+  it ('enter a number and recieve back all allergies within it') do
+    allergies(3).should(eq(['peanuts', 'eggs']))
   end
 end
